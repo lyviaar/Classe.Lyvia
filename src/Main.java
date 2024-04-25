5adf4dd0bd381363fdc0c217cfa5eb3e497df181
lyvia mariah <168114517+lyviaar@users.noreply.github.com>
2024-04-25 16:13:19 -0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String nome = "Mariah";
+        int idade = 17;
+        System.out.println("seu nome é:" + nome);
+        System.out.println("sua idade é:" + idade);
+    }
+}
